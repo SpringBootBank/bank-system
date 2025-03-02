ef2d35ccdf49f317439f6af70b4177e3865eea4b
Julia_Sheveleva <julia_sheveleva@senla.eu>
2025-03-02 21:08:55 +0100

    test commit

--- src/main/java/com/itgirls/bank_system/BankSystemApplication.java
+++ src/main/java/com/itgirls/bank_system/BankSystemApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BankSystemApplication {
-//test
-	//test
+
 	public static void main(String[] args) {
 		SpringApplication.run(BankSystemApplication.class, args);
 	}
