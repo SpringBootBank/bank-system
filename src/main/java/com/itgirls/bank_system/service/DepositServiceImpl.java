110eb2b682d8a6251857b53b0d6f20277ed8fee3
KatherineP3101 <kpanova2k@gmail.com>
2025-03-08 17:31:22 +0100

    добавила Маппер для convertToDto.

--- src/main/java/com/itgirls/bank_system/service/DepositServiceImpl.java
+++ src/main/java/com/itgirls/bank_system/service/DepositServiceImpl.java
@@ -1,8 +1,6 @@
 package com.itgirls.bank_system.service;
 
-import com.itgirls.bank_system.dto.AccountDto;
 import com.itgirls.bank_system.dto.DepositDto;
-import com.itgirls.bank_system.dto.UserDto;
 import com.itgirls.bank_system.enums.DepositStatus;
 import com.itgirls.bank_system.enums.Role;
 import com.itgirls.bank_system.exception.DepositNotFoundException;
@@ -28,6 +26,8 @@ import java.util.NoSuchElementException;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
+
+import static com.itgirls.bank_system.mapper.EntityToDtoMapper.convertDepositToDto;
 
 @Service
 @Slf4j
@@ -64,6 +64,8 @@ public class DepositServiceImpl implements DepositService {
                     "указать пользователя на имя которого открывается вклад.");
         } else if (!user.getId().equals(account.getUser().getId())) {
             throw new IllegalStateException("Этот счет привязан к другому пользователю.");
+        } else if (account.getDeposit() != null) {
+            throw new IllegalStateException("Этот счет уже привязан к другому вкладу.");
         } else {
             newDeposit.setAmountDeposit(depositDto.getAmountDeposit());
             newDeposit.setInterestRateDeposit(depositDto.getInterestRateDeposit());
@@ -212,36 +214,5 @@ public class DepositServiceImpl implements DepositService {
                 return "Вклад с ID " + id + " был удален.";
             }
         }
-    }
-
-    private DepositDto convertDepositToDto(Deposit deposit) {
-        log.debug("Трансформация вклада с ID {} в DTO.", deposit.getId());
-        return DepositDto.builder()
-                .id(deposit.getId())
-                .account(convertAccountToDto(deposit.getAccount()))
-                .user(convertUserToDto(deposit.getUser()))
-                .amountDeposit(deposit.getAmountDeposit())
-                .startDateDeposit(deposit.getStartDateDeposit())
-                .endDateDeposit(deposit.getEndDateDeposit())
-                .interestRateDeposit(deposit.getInterestRateDeposit())
-                .statusDeposit(String.valueOf(deposit.getStatusDeposit()))
-                .build();
-    }
-
-    private UserDto convertUserToDto(User user) {
-        log.debug("Трансформация пользователя с ID {} в DTO.", user.getId());
-        return UserDto.builder()
-                .id(user.getId())
-                .name(user.getName())
-                .surname(user.getSurname())
-                .build();
-    }
-
-    private AccountDto convertAccountToDto(Account account) {
-        log.debug("Трансформация счета с ID {} в DTO.", account.getId());
-        return AccountDto.builder()
-                .id(account.getId())
-                .accountNumber(account.getAccountNumber())
-                .build();
     }
 }
