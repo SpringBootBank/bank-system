065376b15aabcb72cbfbbab62cb458fb0b86212d
bobrisheva <bobrisheva@gmail.com>
2025-03-09 12:51:08 +0300

    Перенос методов конвертеров из UserServiceImpl в EntityToDtoMapper.

--- src/main/java/com/itgirls/bank_system/mapper/EntityToDtoMapper.java
+++ src/main/java/com/itgirls/bank_system/mapper/EntityToDtoMapper.java
@@ -1,12 +1,10 @@
 package com.itgirls.bank_system.mapper;
 
-import com.itgirls.bank_system.dto.AccountDto;
-import com.itgirls.bank_system.dto.DepositDto;
-import com.itgirls.bank_system.dto.UserDto;
-import com.itgirls.bank_system.model.Account;
-import com.itgirls.bank_system.model.Deposit;
-import com.itgirls.bank_system.model.User;
+import com.itgirls.bank_system.dto.*;
+import com.itgirls.bank_system.model.*;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.stream.Collectors;
 
 @Slf4j
 public class EntityToDtoMapper {
@@ -39,6 +37,81 @@ public class EntityToDtoMapper {
         return AccountDto.builder()
                 .id(account.getId())
                 .accountNumber(account.getAccountNumber())
+                .build();
+    }
+
+    public static UserDto convertNewUserToDto(User user) {
+        log.debug("Трансформация созданного пользователя с ID {} в DTO", user.getId());
+        return UserDto.builder()
+                .id(user.getId())
+                .email(user.getEmail())
+                .role(user.getRole())
+                .name(user.getName())
+                .surname(user.getSurname())
+                .build();
+    }
+
+    public static UserDto convertEveryUserToDto(User user) {
+        log.debug("Трансформация пользователей в DTO");
+        if (user == null) {
+            log.error("Пользователь с пустыми полями не может быть создан");
+            return null;
+        }
+        return UserDto.builder()
+                .id(user.getId())
+                .email(user.getEmail())
+                .role(user.getRole())
+                .name(user.getName())
+                .surname(user.getSurname())
+                .accounts(user.getAccounts().stream().map(EntityToDtoMapper::convertAccountsToDto).collect(Collectors.toSet()))
+                .deposits(user.getDeposits().stream().map(EntityToDtoMapper::convertDepositsToDto).collect(Collectors.toSet()))
+                .loans(user.getLoans().stream().map(EntityToDtoMapper::convertLoansToDto).collect(Collectors.toSet()))
+                .transactions(user.getTransactions().stream().map(EntityToDtoMapper::convertTransactionsToDto).collect(Collectors.toSet()))
+                .build();
+    }
+
+    public static AccountDto convertAccountsToDto(Account account) {
+        log.debug("Трансформация счета с ID {} в DTO", account.getId());
+        return AccountDto.builder()
+                .id(account.getId())
+                .accountNumber(account.getAccountNumber())
+                .type(account.getType().name())
+                .build();
+    }
+
+    private static DepositDto convertDepositsToDto(Deposit deposit) {
+        log.debug("Трансформация депозита с ID {} в DTO", deposit.getId());
+        return DepositDto.builder()
+                .id(deposit.getId())
+                .statusDeposit(deposit.getStatusDeposit().name())
+                .interestRateDeposit(deposit.getInterestRateDeposit())
+                .startDateDeposit(deposit.getStartDateDeposit())
+                .endDateDeposit(deposit.getEndDateDeposit())
+                .build();
+    }
+
+    private static LoanDto convertLoansToDto(Loan loan) {
+        log.debug("Трансформация кредита с ID {} в DTO", loan.getId());
+        return LoanDto.builder()
+                .id(loan.getId())
+                .statusLoan(loan.getStatusLoan().name())
+                .amountLoan((loan.getAmountLoan()))
+                .monthlyPayment(loan.getMonthlyPayment())
+                .startDateLoan(loan.getStartDateLoan())
+                .endDateLoan(loan.getEndDateLoan())
+                .build();
+    }
+
+    private static TransactionDto convertTransactionsToDto(Transactions transactions) {
+        log.debug("Трансформация транзакции с ID {} в DTO", transactions.getId());
+        return TransactionDto.builder()
+                .id(transactions.getId())
+                .transactionNumber(transactions.getTransactionNumber())
+                .transactionType(transactions.getTransactionType().name())
+                .transactionAmount(transactions.getTransactionAmount())
+                .transactionTime(transactions.getTransactionTime())
+                .senderAccountId(transactions.getSenderAccount().getId())
+                .beneficiaryAccountId(transactions.getBeneficiaryAccount().getId())
                 .build();
     }
 }
--- src/main/java/com/itgirls/bank_system/service/UserServiceImpl.java
+++ src/main/java/com/itgirls/bank_system/service/UserServiceImpl.java
@@ -1,14 +1,9 @@
 package com.itgirls.bank_system.service;
 
 import com.itgirls.bank_system.dto.*;
-import com.itgirls.bank_system.enums.TransactionType;
+import com.itgirls.bank_system.mapper.EntityToDtoMapper;
 import com.itgirls.bank_system.model.*;
 import com.itgirls.bank_system.repository.UserRepository;
-import jakarta.persistence.*;
-import jakarta.validation.constraints.Digits;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
@@ -17,11 +12,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -50,12 +42,17 @@ public class UserServiceImpl implements UserService {
         user.setRole(userCreateDto.getRole());
         user.setPassword(passwordEncode);
         userRepository.save(user);
-        return convertNewUserToDto(user);
+        return EntityToDtoMapper.convertNewUserToDto(user);
     }
 
     @Override
     public List<UserDto> getAllUsers() {
-        return userRepository.findAll().stream().map(this::convertUserToDto).collect(Collectors.toList());
+        List<User> allUsers = userRepository.findAll();
+        List<UserDto> allUsersDto = new ArrayList<>();
+        for (User user : allUsers) {
+            allUsersDto.add(EntityToDtoMapper.convertEveryUserToDto(user));
+        }
+        return allUsersDto;
     }
 
     @Override
@@ -92,13 +89,13 @@ public class UserServiceImpl implements UserService {
             user.setTransactions(userUpdateDto.getTransactions());
         }
         userRepository.save(user);
-        return convertUserToDto(user);
+        return EntityToDtoMapper.convertEveryUserToDto(user);
     }
 
     @Override
     public UserDto getUserByID(Long id) {
         User foundUser = userRepository.findUserById(id);
-        return convertUserToDto(foundUser);
+        return EntityToDtoMapper.convertEveryUserToDto(foundUser);
     }
 
     @Override
@@ -111,72 +108,5 @@ public class UserServiceImpl implements UserService {
                 throw new RuntimeException(e);
             }
         } else return "Пользователь с идентификатором " + id + " не найден";
-    }
-
-    private UserDto convertNewUserToDto(User user) {
-        return UserDto.builder()
-                .id(user.getId())
-                .email(user.getEmail())
-                .role(user.getRole())
-                .name(user.getName())
-                .surname(user.getSurname())
-                .build();
-    }
-
-    private UserDto convertUserToDto(User user) {
-        return UserDto.builder()
-                .id(user.getId())
-                .email(user.getEmail())
-                .role(user.getRole())
-                .name(user.getName())
-                .surname(user.getSurname())
-                .accounts(user.getAccounts().stream().map(this::convertAccountToDto).collect(Collectors.toSet()))
-                .deposits(user.getDeposits().stream().map(this::convertDepositToDto).collect(Collectors.toSet()))
-                .loans(user.getLoans().stream().map(this::convertLoanToDto).collect(Collectors.toSet()))
-                .transactions(user.getTransactions().stream().map(this::convertTransactionToDto).collect(Collectors.toSet()))
-                .build();
-    }
-
-
-    private AccountDto convertAccountToDto(Account account) {
-        AccountDto accountDto = AccountDto.builder()
-                .id(account.getId())
-                .accountNumber(account.getAccountNumber())
-                .type(account.getType().name())
-                .build();
-        return accountDto;
-    }
-
-    private DepositDto convertDepositToDto(Deposit deposit) {
-        return DepositDto.builder()
-                .id(deposit.getId())
-                .statusDeposit(deposit.getStatusDeposit().name())
-                .interestRateDeposit(deposit.getInterestRateDeposit())
-                .startDateDeposit(deposit.getStartDateDeposit())
-                .endDateDeposit(deposit.getEndDateDeposit())
-                .build();
-    }
-
-    private LoanDto convertLoanToDto(Loan loan) {
-        return LoanDto.builder()
-                .id(loan.getId())
-                .statusLoan(loan.getStatusLoan().name())
-                .amountLoan((loan.getAmountLoan()))
-                .monthlyPayment(loan.getMonthlyPayment())
-                .startDateLoan(loan.getStartDateLoan())
-                .endDateLoan(loan.getEndDateLoan())
-                .build();
-    }
-
-    private TransactionDto convertTransactionToDto(Transactions transactions) {
-        return TransactionDto.builder()
-                .id(transactions.getId())
-                .transactionNumber(transactions.getTransactionNumber())
-                .transactionType(transactions.getTransactionType().name())
-                .transactionAmount(transactions.getTransactionAmount())
-                .transactionTime(transactions.getTransactionTime())
-                .senderAccountId(transactions.getSenderAccount().getId())
-                .beneficiaryAccountId(transactions.getBeneficiaryAccount().getId())
-                .build();
     }
 }
